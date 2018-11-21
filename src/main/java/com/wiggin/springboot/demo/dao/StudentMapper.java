72db490ed0f75274fbeee815439e0cfe22ff975a
guwy <guwy@chineseall.com>
2018-11-21 10:05:08 +0800

    新建demo

---
+++ src/main/java/com/wiggin/springboot/demo/dao/StudentMapper.java
@@ -0,0 +1,21 @@
+package com.wiggin.springboot.demo.dao;
+
+import com.wiggin.springboot.demo.model.Student;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    Student selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
+
+
+
+v2v2v2v2v2v2v2v2v2v2v2v2v
